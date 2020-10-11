e78b8323f304b5b34644c33c70eb73fed63c9e9c
Khakhlova <khokhlova.katherina@gmail.com>
2020-10-12 01:03:57 +0300

    feat: update brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let newstr = str.split('');
+    let stack = [];
+    let bracket = new Map(bracketsConfig);
+
+    for (let i = 0; i < newstr.length; i++) {
+        let br = newstr[i];
+        if (br === bracket.get(stack[stack.length - 1])) {
+            if (stack.length == 0) {
+                return false;
+            } else {
+                stack.pop();
+            };
+        } else if (bracket.has(br)) {
+            stack.push(br);
+        } else {
+            return false;
+        };
+    };
+
+    return stack.length === 0;;
+};
